67788c2de2342e57e1af199eaae6ff13297a97ee
irg1008 <ivansudevlop@gmail.com>
2022-09-06 13:06:29 +0200

    Added api ket protection for all routes under api

--- src/db/client.py
+++ src/db/client.py
@@ -1,9 +1,20 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from motor.core import AgnosticDatabase
+from motor.motor_asyncio import (
+    AsyncIOMotorClient,
+    AsyncIOMotorDatabase,
+    AsyncIOMotorCollection,
+)
+from motor.core import AgnosticDatabase, AgnosticCollection
 from decouple import config
+from typing import TypedDict
 
 client = AsyncIOMotorClient(config("MONGO_URI"))
 
 # Using agnostic for typing due to async not having type completion. 😿
 
-db: AgnosticDatabase = client["yoda-db"]
+db: AsyncIOMotorDatabase = client["yoda-db"]
+
+
+api_keys: AsyncIOMotorCollection = db["api_keys"]
+generations: AsyncIOMotorCollection = db["generations"]
+suggestions: AsyncIOMotorCollection = db["suggestions"]
+users: AsyncIOMotorCollection = db["users"]
