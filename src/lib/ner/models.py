8b08b33238bc216ab2951f82e2338b2018584977
irg1008 <ivansudevlop@gmail.com>
2022-09-06 10:12:56 +0200

    Fixed camel case on response examples

--- src/lib/ner/models.py
+++ src/lib/ner/models.py
@@ -1,18 +1,11 @@
 from typing import Optional, Union
+
+from pydantic import Field
 from utils.models import Model
 
 Entity = list[Union[str, int]]
 
 
 class Entities(Model):
-    color: Optional[Entity]
-    size: Optional[Entity]
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "color": ["red", "blue"],
-                "size": ["s", "l", "43.5"],
-                "brand": ["Zara", "Adidas"],
-            }
-        }
+    color: Optional[Entity] = Field(example=["red", "blue"])
+    size: Optional[Entity] = Field(example=["small", "large", "43,5"])
