5062f12fef4b57b819e708f586dca0c0e7e10de2
irg1008 <ivansudevlop@gmail.com>
2022-09-07 12:48:04 +0200

    Little change to test container cache and continous development

--- src/main.py
+++ src/main.py
@@ -9,14 +9,7 @@ app.include_router(router)
 # Origins for development and production clients.
 origins = ["http://localhost:3000", "https://app.lighthousefeed.com"]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["GET"],
-    allow_headers=["*"],
-    max_age=3600,
-)
+app.add_middleware(CORSMiddleware, allow_origins=origins)
 
 
 @app.get("/", include_in_schema=False)
