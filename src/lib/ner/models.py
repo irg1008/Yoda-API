99ce3c2cce9cb5341ae51f8e7304ad8ee212d9e4
irg1008 <ivansudevlop@gmail.com>
2022-09-14 10:24:15 +0200

    Colors antity are now string only

--- src/lib/ner/models.py
+++ src/lib/ner/models.py
@@ -3,9 +3,10 @@ from typing import Optional, Union
 from pydantic import Field
 from utils.models import Model
 
-Entity = list[Union[str, int]]
+Color = list[str]
+Size = list[Union[str, int]]
 
 
 class Entities(Model):
-    color: Optional[Entity] = Field(example=["red", "blue"])
-    size: Optional[Entity] = Field(example=["small", "large", "43,5"])
+    color: Optional[Color] = Field(example=["red", "blue"])
+    size: Optional[Size] = Field(example=["small", "large", "43,5"])
