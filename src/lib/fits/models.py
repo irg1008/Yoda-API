86de582f7f6ca895fdb85074cf8dc4299ccf3ea4
irg1008 <ivansudevlop@gmail.com>
2022-10-10 11:59:28 +0200

    Added fits models back until production release

--- src/lib/fits/models.py
+++ src/lib/fits/models.py
@@ -4,5 +4,11 @@ from pydantic import Field
 from utils.models import Model
 
 
+class PriceUnit(str, Enum):
+    USD = "USD"
+
+
 class Completion(Model):
     title: str = Field(example="A short title given a much longer one")
+    estimated_price: float = Field(example=4e-5)
+    price_unit: PriceUnit = Field(default=PriceUnit.USD, example=PriceUnit.USD)
