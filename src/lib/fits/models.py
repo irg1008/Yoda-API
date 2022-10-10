a05f5c6e74f5f3bf5bdd7f39d449452d65170cf2
irg1008 <ivansudevlop@gmail.com>
2022-10-10 10:40:09 +0200

    Removed price data on fits endpoint

--- src/lib/fits/models.py
+++ src/lib/fits/models.py
@@ -4,11 +4,5 @@ from pydantic import Field
 from utils.models import Model
 
 
-class PriceUnit(str, Enum):
-    USD = "USD"
-
-
 class Completion(Model):
     title: str = Field(example="A short title given a much longer one")
-    estimated_price: float = Field(example=4e-5)
-    price_unit: PriceUnit = Field(default=PriceUnit.USD, example=PriceUnit.USD)
