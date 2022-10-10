0f1159dc1e16c3a527631fa7ee541c099f0ec494
irg1008 <ivansudevlop@gmail.com>
2022-10-10 12:11:18 +0200

    Revert to model without prices

--- src/lib/fits/models.py
+++ src/lib/fits/models.py
@@ -3,12 +3,5 @@ from enum import Enum
 from pydantic import Field
 from utils.models import Model
 
-
-class PriceUnit(str, Enum):
-    USD = "USD"
-
-
 class Completion(Model):
     title: str = Field(example="A short title given a much longer one")
-    estimated_price: float = Field(example=4e-5)
-    price_unit: PriceUnit = Field(default=PriceUnit.USD, example=PriceUnit.USD)
